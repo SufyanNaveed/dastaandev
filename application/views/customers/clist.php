2d592cab65f61dd228e14ce40cf8f37ecff9d9c8
Naveed <naveed.rehman@cache-cloud.com>
2022-04-13 17:15:45 +0500

    sum of due

--- application/views/customers/clist.php
+++ application/views/customers/clist.php
@@ -20,7 +20,7 @@ if ($this->input->get('due')) {
             
             <div style="text-align:center;"> 
                 <?php if(is_array($sum_due) && count($sum_due) > 0){ ?>
-                    DUE SUM: <b><?php echo $sum_due['total']; ?></b>
+                    DUE SUM: <b><?php echo 'Rs: '. number_format($sum_due['total']); ?></b>
                 <?php } ?>
             </div>
             <div class="heading-elements">
